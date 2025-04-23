75e74db0c295058bfc4fd1c4528b46c860bc8f08
Thinal Dilmith <thinaldilmith3@gmail.com>
2025-04-23 19:25:38 +0530

    upload welcome img

--- src/GUI_interface.py
+++ src/GUI_interface.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from PIL import Image
 
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
@@ -9,7 +10,21 @@ class App(ctk.CTk):# Inherit from CTk main window
        super().__init__()
        self.title("My Manager")
        self.geometry("600x600")
- 
+
+       #create frames
+       self.welcome=ctk.CTkFrame(self)
+       self.main_frame=ctk.CTkFrame(self)
+
+       # set up the frames of GUI
+       self.setup_welcome()
+       self.setup_mainframe()
+    
+    def setup_welcome():
+        #create img to welcome frame
+        welcome_img=ctk.CTkImage(light_image=Image.open(""),dark_image=Image.open("img/welcome.png"))
+       
+        
+
 
 
 
