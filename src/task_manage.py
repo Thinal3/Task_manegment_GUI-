a36a5f731b3d32b902ad3dc732542592a0e9ee82
Thinal Dilmith <thinaldilmith3@gmail.com>
2025-04-28 03:09:17 +0530

    create Task manage class
    Added the add_task method to the Task_manage class, which automatically generates a unique task ID and adds a new task to the tasks_dict.
    
    Implemented load_file method to load tasks from tasks.json into tasks_dict.
    
    Implemented save_file method to save the updated tasks_dict back to tasks.json after adding a task.

--- src/task_manage.py
+++ src/task_manage.py
@@ -1,2 +1,27 @@
+import json
 
+class Task_manage:
+    def __init__(self):
+        self.tasks_dict={}
+        self.load_file()
 
+    def add_task(self,name,description,priority,date):
+        task_id=len(self.tasks_dict)+1
+        
+        self.tasks_dict[task_id]={
+            "task_name":name,
+            "description":description,
+            "priority":priority,
+            "date":date
+        }
+        self.save_file()
+    
+    def load_file(self):
+        with open("tasks.json","r") as file:
+            self.tasks_dict=json.load(file)
+        
+    def save_file(self):
+        with open("tasks.json", "w") as file:
+            json.dump(self.tasks_dict, file, indent=4)
+
+    
